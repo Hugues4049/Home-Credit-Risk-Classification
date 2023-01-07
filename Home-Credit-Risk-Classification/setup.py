2ea6fe517c8300e38611cfe8629e0d4e5b9693cf
Hugues4049 <titoebene@gmail.com>
2023-01-07 02:19:30 +0100

    creation of a cookiecutter

---
+++ Home-Credit-Risk-Classification/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The goal of this project is to apply some concepts & tools like ML project, MLFlow, classification...',
+    author='Hugues Roland EBENE',
+    license='MIT',
+)
